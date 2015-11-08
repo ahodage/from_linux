29096bb805da2e46ff8bae53f6e3e29e49183dab
ahodage <abhishekhodage@gmail.com>
2015-11-08 10:17:10 -0800

    CPP codes

--- perl__/pattern.pl
+++ perl__/pattern.pl
@@ -3,11 +3,22 @@
 use warnings;
 use strict;
 
+=pot
 print "Input a string to in which you want to find pattern\n";
-$_ = <STDIN>;
-chomp();
-my $z = m/\s*the+\s/g;
-print "$z\n";
+my $x;
+#$x = <STDIN>;
+$x = "Novice to Expert #@ in %^ a 3 day Perl course.\n";
+chomp($x);
+my $count;
+while (my $z = ($x =~ /(\W+)/g))
+{
+	print "$1 __"; 
+	$count++;
+}
+print "$count\n";
 
 #grep function is used to extract a the pattern that matches
-
+=cut
+my $text = "!random 3d4+5-6";
+my @matches = $text =~ /((?:\+|-)?\d)/g;
+print "@matches\n";
